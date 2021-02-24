3ee2ca0eefbfb257488958eac22f4156521bc2a7
hossamhaddad <husamhaddad80@gmail.com>
2021-02-24 20:20:56 +0200

    fixing ssh

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ const pg=require('pg');
 
 const superagent = require('superagent');
 
-const client = new pg.Client(process.env.DATABASE_URL);
+// const client = new pg.Client(process.env.DATABASE_URL);
 
-// const client = new pg.Client({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false } });
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false } });
 
 // server.listen(PORT, () => {
 //   console.log(`listening on port ${PORT}`)
