37b37dca6399155cde77cac47e2f5df16068e8f3
hossamhaddad <husamhaddad80@gmail.com>
2021-02-27 17:05:24 +0200

    fixing server

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const pg=require('pg');
 
 const superagent = require('superagent');
 
-// const client = new pg.Client(process.env.DATABASE_URL);
+      // const client = new pg.Client(process.env.DATABASE_URL);
 
 const client = new pg.Client({ connectionString: process.env.DATABASE_URL,ssl: { rejectUnauthorized: false } });
 
